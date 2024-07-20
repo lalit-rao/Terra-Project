c8d467be58024c683beba6ca5c2401bc6347c588
Lalit Rao <127052986+lalit-rao@users.noreply.github.com>
2024-07-20 06:30:43 +0530

    updated code

---
+++ frontend/public/images/index.js
@@ -0,0 +1,15 @@
+import chatbox from "./chatbox-icon.svg"
+import aiBot from "./ai-bot.png"
+import bot from "./bot.jpg"
+import gradient from "./gradient.jpg"
+import message from "./message.png"
+import message2 from "./message-2.png"
+
+export {
+    chatbox,
+    aiBot,
+    bot,
+    gradient,
+    message,
+    message2,
+}
