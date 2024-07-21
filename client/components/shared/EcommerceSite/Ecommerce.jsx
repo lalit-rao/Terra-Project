8208492f6b1b18848b7569143c2f25b9bbe4a812
Lalit Rao <127052986+lalit-rao@users.noreply.github.com>
2024-07-21 13:22:37 +0530

    updated code

--- client/components/shared/EcommerceSite/Ecommerce.jsx
+++ client/components/shared/EcommerceSite/Ecommerce.jsx
@@ -7,7 +7,7 @@ import SearchAlsoCountryCard from "./Cards/SearchAlsoCountryCard";
 import FilterCard from "./Cards/FilterCard";
 import { Fade } from "react-awesome-reveal";
 import Image from "next/image";
-import {bestSeller2, tree3, tree4, tree1, tree2, tree5} from "../../../public/images";
+import { bestSeller2, tree3, tree4, tree1, tree2, tree5 } from "../../../public/images";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -32,8 +32,7 @@ function App() {
           product_condition: "ALL",
         },
         headers: {
-          "x-rapidapi-key":
-            "d2c3d2f471msh515b08e24ff0115p1c83e0jsndb5ad18e7a9f",
+          "x-rapidapi-key": "d2c3d2f471msh515b08e24ff0115p1c83e0jsndb5ad18e7a9f",
           "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com",
         },
       };
@@ -89,68 +88,45 @@ function App() {
       </div>
 
       <div className="pt-15 relative h-[100vh] flex items-center place-content-center flex-col gap-10">
-        <h1 className="text-3xl md:text-5xl  lg:text-8xl text-[#191A23] font-bold z-10">
+        <h1 className="text-3xl md:text-5xl lg:text-8xl text-[#191A23] font-bold z-10">
           Marketplace
         </h1>
         <p className=" text-xl lg:text-4xl text-[#8A868C] z-10 ">
           Explore a curated selection of sustainable products
         </p>
         <div className="">
-          <Image
-            src={tree1}
-            alt=""
-            className="absolute top-0 left-0 h-40 md:h-80 "
-          />
-          <Image
-            src={tree2}
-            alt=""
-            className="absolute left-0 bottom-0 h-20 md:h-80"
-          />
-          <Image
-            src={tree3}
-            alt=""
-            className="absolute right-0 bottom-0 h-10  lg:h-52"
-          />
-          <Image
-            src={tree4}
-            alt=""
-            className="absolute right-0 top-0 h-18 md:h-full"
-          />
-          <Image
-            src={tree5}
-            alt=""
-            className="absolute bottom-0 right-1/2 h-20 md:h-52"
-          />
+          <Image src={tree1} alt="" className="absolute top-0 left-0 h-40 md:h-80 " />
+          <Image src={tree2} alt="" className="absolute left-0 bottom-0 h-20 md:h-80" />
+          <Image src={tree3} alt="" className="absolute right-0 bottom-0 h-10 lg:h-52" />
+          <Image src={tree4} alt="" className="absolute right-0 top-0 h-18 md:h-full" />
+          <Image src={tree5} alt="" className="absolute bottom-0 right-1/2 h-20 md:h-52" />
         </div>
       </div>
 
       <div className="flex justify-center text-center h-[90vh] place-content-center">
         <div className="w-[45vw] text-left flex flex-col place-content-center gap-2 md:gap-5">
-          <h1 className="text-sm md:text-4xl font-bold text-[#191A23]">
-            Why choose us?
-          </h1>
+          <h1 className="text-sm md:text-4xl font-bold text-[#191A23]">Why choose us?</h1>
           <div className="h-1 w-28 md:h-2 bg-[#78b829] rounded-md"></div>
           <div className="text-[10px] md:text-2xl text-[#8A868C] flex flex-col gap-3">
             <Fade>
               <p>
-                🌱<b>Global Selection:</b> Access a diverse range of sustainable
-                products sourced from 22 countries, ensuring quality and variety
-                for every eco-conscious shopper.
+                🌱<b>Global Selection:</b> Access a diverse range of sustainable products
+                sourced from 22 countries, ensuring quality and variety for every
+                eco-conscious shopper.
               </p>
             </Fade>
             <Fade>
               <p>
-                🌵
-                <b>Informed Shopping:</b>
-                Make confident purchasing decisions with comprehensive product
-                details highlighting eco-friendly features and benefits.
+                🌵<b>Informed Shopping:</b> Make confident purchasing decisions with
+                comprehensive product details highlighting eco-friendly features and
+                benefits.
               </p>
             </Fade>
             <Fade>
               <p>
-                ☘️<b>Convenient Experience:</b> Enjoy seamless shopping with
-                easy navigation and direct links, making sustainable choices
-                hassle-free and accessible.
+                ☘️<b>Convenient Experience:</b> Enjoy seamless shopping with easy
+                navigation and direct links, making sustainable choices hassle-free and
+                accessible.
               </p>
             </Fade>
           </div>
@@ -159,19 +135,18 @@ function App() {
 
       {searchItem && (
         <div className="mb-10 border-b-2 border-[#D9D9D9] pb-3 md:pb-12 mx-7">
-          <h1 className="text-xl md:text-4xl font-bold  text-[#191A23] my-2 md:my-5 ">
+          <h1 className="text-xl md:text-4xl font-bold text-[#191A23] my-2 md:my-5">
             Search Results:
           </h1>
           <div className="w-full flex justify-center mb-3 md:mb-10">
             <div className="h-[4px] md:h-2 w-20 bg-[#78b829] rounded-md"></div>
           </div>
-          <div className="flex flex-wrap  justify-center md:my-6 ">
+          <div className="flex flex-wrap justify-center md:my-6">
             {filteredProducts.length > 0 ? (
               filteredProducts.map((product) => (
-                <Fade>
+                <Fade key={product.product_url}>
                   <div
                     className="flex flex-wrap justify-around m-2 md:m-7 relative gap-2 md:gap-4"
-                    key={product.product_url}
                   >
                     <Card
                       title={product.product_title}
@@ -193,7 +168,7 @@ function App() {
       )}
 
       <div>
-        <h1 className="text-xl md:text-4xl font-bold  text-[#191A23] my-2 md:my-5 ">
+        <h1 className="text-xl md:text-4xl font-bold text-[#191A23] my-2 md:my-5">
           All Products:
         </h1>
         <div className="w-full flex justify-center mb-3 md:mb-10">
@@ -204,13 +179,7 @@ function App() {
           <div className="bg-[#191A23] md:px-5 border md:text-lg text-[10px] px-2 py-[1px] rounded-lg border-black text-[#F3F3F3] md:rounded-2xl max-w-max md:py-2">
             Filter🔎
           </div>
-          <FilterCard
-            variableFunc={setRate}
-            Name="rate"
-            variable={rate}
-            label="Rating 3.5+"
-          />
-
+          <FilterCard variableFunc={setRate} Name="rate" variable={rate} label="Rating 3.5+" />
           <FilterCard
             variableFunc={setNoOfRating}
             Name="rating no"
@@ -219,22 +188,14 @@ function App() {
           />
         </div>
 
-        <div className="flex flex-wrap  justify-center my-6">
+        <div className="flex flex-wrap justify-center my-6">
           {filteredData.length > 0 ? (
             filteredData.map((product) => (
-              <Fade>
+              <Fade key={product.product_url}>
                 <div
                   className="flex flex-wrap justify-around m-2 md:m-7 relative gap-2 md:gap-4"
-                  key={product.product_url}
                 >
-                  {product.is_prime && (
-                    <div className="">
-                      <Image
-                        src={bestSeller2}
-                        className="absolute top-0 right-0 h-6 w-6 md:h-14 md:w-14  z-20"
-                      />
-                    </div>
-                  )}
+                  {product.is_prime && <p className="absolute top-0 left-0">Amazon Prime</p>}
                   <Card
                     title={product.product_title}
                     product_photo={product.product_photo}
@@ -248,7 +209,7 @@ function App() {
               </Fade>
             ))
           ) : (
-            <div>No products found</div>
+            <div className="text-[#191A23]">No products found</div>
           )}
         </div>
       </div>
